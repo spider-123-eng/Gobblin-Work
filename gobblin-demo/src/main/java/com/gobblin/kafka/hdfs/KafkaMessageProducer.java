df2acfb2289154a398f35c98fe79cb4c0882f684
Revanth Reddy <revanthkumar95@gmail.com>
2017-03-22 21:16:15 +0530

    Kafka Message Producer to push data to kafka .

--- gobblin-demo/src/main/java/com/gobblin/kafka/hdfs/KafkaMessageProducer.java
+++ gobblin-demo/src/main/java/com/gobblin/kafka/hdfs/KafkaMessageProducer.java
@@ -1,6 +1,8 @@
 package com.gobblin.kafka.hdfs;
 
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -9,7 +11,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 
 /**
- * Kafka producer to push data as byte[] 
+ * Kafka producer to push data as byte[]
  */
 public class KafkaMessageProducer {
 	public static void main(String[] args) throws InterruptedException {
@@ -25,22 +27,65 @@ public class KafkaMessageProducer {
 		producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
 
 		producer = new KafkaProducer<>(producerProperties);
-
+		int mb = 1024 * 1024;
 		final String topic = AppConfiguration.KAFKA_SERVERS_TOPIC;
-		for (int i = 1; i < 100; i++) {
-			final String json = "" + "{" + "\"timestamp\": \"2015-08-21T17:08:45-0400\"" + ", \"type\": \"SYSTEM\""
-					+ ",\"level\":  " + i + "," + "\"message\": \"RAM usage above 90 per cent utilization\"" + " }"
-					+ "";
+		String pattern = "YYYY-MM-dd:HH:mm:ss";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 
-			ProducerRecord<byte[], byte[]> producerRecord = new ProducerRecord<>(topic,
-					json.getBytes(StandardCharsets.UTF_8));
-			producer.send(producerRecord);
-			// add a delay of 1 seconds
-			Thread.sleep(1000);
+		// get Runtime instance
+		Runtime instance = Runtime.getRuntime();
+		String json = "";
+
+		for (int i = 1; i <= 15; i++) {
+
+			String date = simpleDateFormat.format(new Date());
+			if (i % 4 == 0) {
+				long maxMem = (instance.maxMemory() / mb);
+				json = "" + "{" + "\"timestamp\": \"" + date + "\"" + ", \"type\": \"Heap utilization statistics\""
+						+ ",\"level\":  " + i + "," + "\"message\": \"Max Memory in MB:\"" + maxMem + "}" + "";
+				sendMessage(topic, json, producer);
+			} else if (i % 2 == 0) {
+				long totMem = (instance.totalMemory() / mb);
+				json = "" + "{" + "\"timestamp\": \"" + date + "\"" + ", \"type\": \"Heap utilization statistics\""
+						+ ",\"level\":  " + i + "," + "\"message\": \"Total Memory in MB:\"" + totMem + "}" + "";
+				sendMessage(topic, json, producer);
+			} else if (i % 3 == 0) {
+				long freeMem = (instance.freeMemory() / mb);
+				json = "" + "{" + "\"timestamp\": \"" + date + "\"" + ", \"type\": \"Heap utilization statistics\""
+						+ ",\"level\":  " + i + "," + "\"message\": \"Free Memory in MB:\"" + freeMem + "}" + "";
+				sendMessage(topic, json, producer);
+			} else if (i % 5 == 0) {
+				long usedMem = ((instance.totalMemory() - instance.freeMemory()) / mb);
+				json = "" + "{" + "\"timestamp\": \"" + date + "\"" + ", \"type\": \"Heap utilization statistics\""
+						+ ",\"level\":  " + i + "," + "\"message\": \"Used Memory in MB:\"" + usedMem + "}" + "";
+
+				sendMessage(topic, json, producer);
+
+			} else {
+				long usedMem = instance.availableProcessors();
+				json = "" + "{" + "\"timestamp\": \"" + date + "\"" + ", \"type\": \"Heap utilization statistics\""
+						+ ",\"level\":  " + i + "," + "\"message\": \"Available Processors:\"" + usedMem + "}" + "";
+
+				sendMessage(topic, json, producer);
+
+			}
+			// add a delay of 2 seconds
+			Thread.sleep(2000);
+
 		}
+
 		producer.close();
 
 		System.out.println("DONE!");
+
+	}
+
+	public static void sendMessage(String topic, String json, KafkaProducer<byte[], byte[]> producer)
+			throws InterruptedException {
+
+		ProducerRecord<byte[], byte[]> producerRecord = new ProducerRecord<>(topic,
+				json.getBytes(StandardCharsets.UTF_8));
+		producer.send(producerRecord);
 
 	}
 }
